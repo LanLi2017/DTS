1eaf469dcf3866459d23763f68d88fbff1e34a9b
lanl2 <lanl2@illinois.edu>
2020-02-02 17:18:53 -0600

    DTS_func

--- operation_history_DTS.py
+++ operation_history_DTS.py
@@ -34,13 +34,34 @@ def func_invertible(f,g):
     return func_compose(f,g)==func_compose(g,f)
 
 
-def column_trans_func():
-
+def column_trans_func(op_name,j):
+    # Column transformation means after the transformation,
+    # The whole table deducts the old column and plus the new column after the transformation f.
+    # paper got typo...need edition
+    '''
+    
+    :param op_name: applied column transformation name ("split the column",etc)
+    :param j: index of column
+    :return: new value
+    '''
     pass
 
 
-def column_independent():
-    pass
+def column_independent(f,g,x,y):
+    # Two sequential columns are independent / parallel
+    # f(x)'g(y)=g(y)'f(x)
+    # change the order of the transformation, the results keep consistant
+    '''
+    
+    :param f: operation history applied on column x
+    :param g: operaiton history applied on column y
+    :param x: column index x
+    :param y: column index y
+    :return: true or false
+    '''
+    return func_compose(column_trans_func(f,x),column_trans_func(g,y))== \
+           func_compose(column_trans_func(g,y),column_trans_func(f,x))
+
 
 
 
